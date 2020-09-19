dd38f0ecf0cec602cd4ef8bfee9fba88075cb7b6
ashishsahu1 <ashishsahu1999@hotmail.com>
2020-09-19 21:06:40 +0530

    Some tweaks

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SESSION_COOKIE_SECURE'] = False
+
+@app.route('/')
+def home():
+    return render_template('base.html')
+
+if __name__ == "__main__":
+    app.run(debug = True)
